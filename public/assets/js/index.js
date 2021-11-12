f9aeb39ad6ee075a233e327a7c59f91995609f21
Vince Momot <vincemomot@gmail.com>
2021-11-12 09:28:15 -0500

    added some get requests to server.js

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -4,7 +4,8 @@ let saveNoteBtn;
 let newNoteBtn;
 let noteList;
 
-if (window.location.pathname === '/notes') {
+if (window.location.pathname === '/notes.html') {
+  console.log("line 8")
   noteTitle = document.querySelector('.note-title');
   noteText = document.querySelector('.note-textarea');
   saveNoteBtn = document.querySelector('.save-note');
@@ -16,7 +17,7 @@ if (window.location.pathname === '/notes') {
 const show = (elem) => {
   elem.style.display = 'inline';
 };
-
+show(saveNoteBtn);
 // Hide an element
 const hide = (elem) => {
   elem.style.display = 'none';
@@ -109,6 +110,7 @@ const handleNewNoteView = (e) => {
 };
 
 const handleRenderSaveBtn = () => {
+  console.log("index has loaded")
   if (!noteTitle.value.trim() || !noteText.value.trim()) {
     hide(saveNoteBtn);
   } else {
@@ -119,7 +121,7 @@ const handleRenderSaveBtn = () => {
 // Render the list of note titles
 const renderNoteList = async (notes) => {
   let jsonNotes = await notes.json();
-  if (window.location.pathname === '/notes') {
+  if (window.location.pathname === '/notes.html') {
     noteList.forEach((el) => (el.innerHTML = ''));
   }
 
@@ -165,7 +167,7 @@ const renderNoteList = async (notes) => {
     noteListItems.push(li);
   });
 
-  if (window.location.pathname === '/notes') {
+  if (window.location.pathname === '/notes.html') {
     noteListItems.forEach((note) => noteList[0].append(note));
   }
 };
@@ -173,7 +175,8 @@ const renderNoteList = async (notes) => {
 // Gets notes from the db and renders them to the sidebar
 const getAndRenderNotes = () => getNotes().then(renderNoteList);
 
-if (window.location.pathname === '/notes') {
+if (window.location.pathname === '/notes.html') {
+  console.log("test")
   saveNoteBtn.addEventListener('click', handleNoteSave);
   newNoteBtn.addEventListener('click', handleNewNoteView);
   noteTitle.addEventListener('keyup', handleRenderSaveBtn);
--- server.js
+++ server.js
@@ -1,36 +1,50 @@
-const express = require('express')
-const PORT = 3001
-const data = require('./db/db.json')
-const app = express()
-const fs = require('fs')
-const path = require('path')
-const api = require('./public/assets/js/index')
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
 
-//middleware
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(express.static('public'))
 
-// GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-)
+const app = express();
+let PORT = process.env.PORT || 3001;
+const mainDir = path.join(__dirname,"/public");
 
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+
+app.get('/notes', (req,res) => { 
+    res.sendFile(mainDir,"/notes.html")
+});
+
+app.get('/api/notes', (req,res) => { 
+    res.sendFile(path.join(__dirname,"./db/db.json"))
+});
+
+app.get("*", (req,res) => { 
+    res.sendFile(mainDir,"/index.html")
+})
+
+app.get("/api/notes:id", (req,res) => { 
+    const createdNotes = fs.readFileSync('./db/db.json','utf8')
+    res.json(createdNotes[Number(req.params.id)])
+});
+
+app.post('/api/notes', (req,res ) => {
+    const createdNotes = JSON.parse(fs.readFileSync('./db/db.json','utf8'))
+    const newNote=req.body
+    let noteId = (createdNotes.length).toString()    
+    newNote.id = noteId
+    createdNotes.push(newNote)
+    fs.writeFileSync('./db/db.json',JSON.stringify(createdNotes))
+    console.log("note Saved",newNote)
+    res.json(createdNotes)
+} )
+
+// const noteId = req.params.id
+// let newId = 0;
 
 app.listen(PORT, () =>
-    console.log(`App listening at http://localhost:${PORT} 🚀 `)
-)
-
-//GET middleware
-
-// Routes
-    //home route sends index.html file
-    //notes route sends notes.html -- each one should have its own ID --download a package for this (UUID)
-//API routes
-// GET api notes -- read bd.json
-    // --parse data into JSON
-    //send to front end
-// POST api notes
-
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
 
 
